fe47ce7320129ef218e122765bca4da26a2faed8
nasigorengpattaya <15017420@myrp.edu.sg>
2017-06-01 13:37:20 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/p07_sms_retriever/FragmentSecond.java
@@ -0,0 +1,132 @@
+package sg.edu.rp.c346.p07_sms_retriever;
+
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.PermissionChecker;
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentSecond extends Fragment {
+
+    Button btnGetSms;
+    Button btnEmail;
+    TextView tvSms;
+    EditText etSms;
+    String smsBody;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_fragment_second, container, false);
+
+        tvSms = (TextView) view.findViewById(R.id.tvSMSByWord);
+        btnGetSms = (Button) view.findViewById(R.id.btnGetSMS);
+        btnEmail = (Button) view.findViewById(R.id.btnEmail);
+        etSms = (EditText)view.findViewById(R.id.editTextNumber);
+
+        btnGetSms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int permissionCheck = PermissionChecker.checkSelfPermission
+                        (getContext(), Manifest.permission.READ_SMS);
+
+                if (permissionCheck != PermissionChecker.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(getActivity(),
+                            new String[]{Manifest.permission.READ_SMS}, 0);
+                    // stops the action from proceeding further as permission not
+                    //  granted yet
+                    return;
+                }
+
+
+                Uri uri = Uri.parse("content://sms");
+
+                String[] reqCols = new String[]{"date", "address", "body", "type"};
+
+                // Get Content Resolver object from which to
+                //  query the content provider
+                ContentResolver cr = getActivity().getContentResolver();
+
+                String filter = "body LIKE ?";
+
+                String word = etSms.getText().toString().trim();
+                String[] wordItems = word.split(" ");
+                Random ran = new Random();
+
+                int randomWord = ran.nextInt(wordItems.length);
+
+                String[] filterARGS = {"%" + wordItems[randomWord] + "%"};
+
+                // Fetch SMS Message from Built-in Content Provider
+                Cursor cursor = cr.query(uri, reqCols, filter, filterARGS, null);
+
+                smsBody = "";
+
+                if (cursor.moveToFirst()) {
+                    do {
+                        long dateInMillis = cursor.getLong(0);
+                        String date = (String) DateFormat
+                                .format("dd MMM yyyy h:mm:ss aa", dateInMillis);
+                        String address = cursor.getString(1);
+                        String body = cursor.getString(2);
+                        String type = cursor.getString(3);
+                        if (type.equalsIgnoreCase("1")) {
+                            type = "Inbox:";
+                        } else {
+                            type = "Sent:";
+                        }
+                        smsBody += type + " " + address + "\n at " + date
+                                + "\n\"" + body + "\"\n\n";
+                    } while (cursor.moveToNext());
+                }
+                tvSms.setText(smsBody);
+
+            }
+        });
+
+        btnEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // The action you want this intent to do;
+                // ACTION_SEND is used to indicate sending text
+                Intent email = new Intent(Intent.ACTION_SEND);
+                // Put essentials like email address, subject & body text
+                email.putExtra(Intent.EXTRA_EMAIL,
+                        new String[]{"jason_lim@rp.edu.sg"});
+                email.putExtra(Intent.EXTRA_SUBJECT,
+                        "P07 SMS Retriever");
+                email.putExtra(Intent.EXTRA_TEXT,
+                        smsBody);
+                // This MIME type indicates email
+                email.setType("message/rfc822");
+                // createChooser shows user a list of app that can handle
+                // this MIME type, which is, email
+                startActivity(Intent.createChooser(email,
+                        "Choose an Email client :"));
+            }
+        });
+
+        return view;
+    }
+
+}
